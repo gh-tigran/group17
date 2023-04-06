ff6a4f0c3dde370a9306e01ef4e3a800dd8004f8
Tigran <tigran.inbox@gmail.com>
2023-04-06 19:38:43 +0400

    change registration validator

--- schema/usersSchema.js
+++ schema/usersSchema.js
@@ -6,8 +6,8 @@ export default {
       firstName: Joi.string().pattern(/^[a-zA-Z]+$/).trim().required(),
       lastName: Joi.string().pattern(/^[a-zA-Z]+$/).trim().required(),
       email: Joi.string().email().required(),
-      password: Joi.string().trim().min(5),
-      cityId: Joi.number().integer().positive().required(),
+      password: Joi.string().trim().min(8),
+      cityId: Joi.number().string(),
     },
   },
   login: {
